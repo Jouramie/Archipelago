2c735147e5038b6748e593826e26c3dd8bee9b15
Jouramie <jouramie@hotmail.com>
2024-07-29 23:15:16 -0400

    fix algo

--- worlds/stardew_valley/stardew_rule/graph.py
+++ worlds/stardew_valley/stardew_rule/graph.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
-from collections import deque
+import logging
 from dataclasses import dataclass
 from functools import cached_property, singledispatch
-from typing import Optional, Tuple, Union, Dict, Hashable, Set, Deque, List
+from typing import Optional, Tuple, Union, Dict, Hashable, Set, List, cast
 
 import networkx as nx
 
 from BaseClasses import CollectionState
 from .base import ShortCircuitPropagation, CombinableStardewRule, Or, And, AssumptionState, BaseStardewRule
-from .literal import LiteralStardewRule
 from .protocol import StardewRule
-from .state import HasProgressionPercent, Reach, Received
+from .state import Received, Reach, HasProgressionPercent
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -27,6 +28,18 @@ class Node:
     @cached_property
     def is_leaf(self):
         return self.true_edge is self.false_edge is None
+
+    def __str__(self, depth=0):
+        if self.is_leaf:
+            return f"{self.rule}"
+
+        padding = '  ' * depth
+        return (f"{self.rule} \n"
+                f"{padding} T {self.true_edge.__str__(depth=depth + 1)}\n"
+                f"{padding} F {self.false_edge.__str__(depth=depth + 1)}")
+
+    def __repr__(self):
+        return self.__str__()
 
 
 @dataclass(frozen=True)
@@ -44,11 +57,11 @@ class Edge:
     def simple_edge(node: Node):
         return Edge((0, 0), 0, [], node)
 
-    def __str__(self):
+    def __str__(self, depth: int = 0):
         leftovers_points = sum(x[1] for x in self.leftovers)
-        return (f"{{{'+' if self.points > 0 else ''}{self.points} -> "
-                f"{self.current_state} + {leftovers_points} leftovers"
-                f"{' [LEAF]' if self.node.is_leaf else ''}}}")
+        enter = '\n'
+        return (f"{{{'+' if self.points > 0 else ''}{self.points} -> {self.current_state} + {leftovers_points} leftovers"
+                f" {self.node.__str__(depth=depth + 1)}}}")
 
     def __repr__(self):
         return self.__str__()
@@ -105,150 +118,142 @@ class ShortCircuitScore:
         return self.true + self.false
 
 
-# TODO split finding root and recursive part
+def to_rule_map(rule: StardewRule) -> nx.DiGraph:
+    """Converts a rule to a graph representation.
+    priority: rules with higher priority are to evaluate first. Priority is assigned per rule type, based on how long it takes to evaluate it.
+    propagation: which result are propagated between rules. Short circuit goes from the starting node to the ending node.
+    scores: the percentage of the rule that will be resolved when this rule is evaluated.
+    """
+    combinable_rules = {}
+    rule_map = _recursive_to_rule_map(rule, nx.DiGraph(), ShortCircuitScore(1, 1), combinable_rules, True)
+    _propagate_combinable_scores(rule_map, combinable_rules)
+    return rule_map
+
+
 @singledispatch
-def to_rule_map(
+def _recursive_to_rule_map(
         rule: StardewRule,
-        _rule_map: nx.DiGraph = None,
-        _score=ShortCircuitScore(1, 1),
-        _combinable_rules: Dict[Hashable, Set[CombinableStardewRule]] = None
+        rule_map: nx.DiGraph,
+        score,
+        combinable_rules: Dict[Hashable, Set[CombinableStardewRule]],
+        root: bool = False
 ) -> nx.DiGraph:
     """Converts a rule to a graph representation.
     priority: rules with higher priority are to evaluate first. Priority is assigned per rule type, based on how long it takes to evaluate it.
     propagation: which result are propagated between rules. Short circuit goes from the starting node to the ending node.
     scores: the percentage of the rule that will be resolved when this rule is evaluated.
     """
-    if _rule_map is None:
-        _rule_map = nx.DiGraph()
-    elif rule in _rule_map.nodes:
-        _rule_map.nodes[rule]["score"] += _score
-        return _rule_map
+    logger.warning(f"Rule {rule} is not supported by the rule map. It will be evaluated afterward.")
+    if rule in rule_map.nodes:
+        rule_map.nodes[rule]["score"] += score
+        return rule_map
 
-    _rule_map.add_node(rule, priority=0, score=_score)
-
-    return _rule_map
+    rule_map.add_node(rule, priority=0, score=score)
+    return rule_map
 
 
-@to_rule_map.register
+@_recursive_to_rule_map.register
 def _(
         rule: And,
-        _rule_map: nx.DiGraph = None,
-        _score=ShortCircuitScore(1, 1),
-        _combinable_rules: Dict[Hashable, Set[CombinableStardewRule]] = None
+        rule_map: nx.DiGraph,
+        score,
+        combinable_rules: Dict[Hashable, Set[CombinableStardewRule]],
+        root: bool = False
 ) -> nx.DiGraph:
-    root = False
-    if _rule_map is None:
-        _rule_map = nx.DiGraph()
-        _combinable_rules = {}
-        root = True
-
-    if rule in _rule_map.nodes:
-        _rule_map.nodes[rule]["score"] += _score
+    if rule in rule_map.nodes:
+        rule_map.nodes[rule]["score"] += score
     else:
-        _rule_map.add_node(rule, priority=0, score=_score)
+        rule_map.add_node(rule, priority=0, score=score, root=root)
 
     subrules = rule.original_rules
-    propagated_score = ShortCircuitScore(_score.min / len(subrules), _score.false)
+    propagated_score = ShortCircuitScore(score.min / len(subrules), score.false)
     for subrule in subrules:
         # TODO simplify subrule while adding in state, with the assumption that other subrules did not short-circuit
-        to_rule_map(subrule, _rule_map, propagated_score, _combinable_rules)
-        _rule_map.add_edge(subrule, rule, propagation=ShortCircuitPropagation.NEGATIVE)
+        _recursive_to_rule_map(subrule, rule_map, propagated_score, combinable_rules)
+        rule_map.add_edge(subrule, rule, propagation=ShortCircuitPropagation.NEGATIVE)
 
-    if root:
-        _propagate_combinable_scores(_rule_map, _combinable_rules)
-
-    return _rule_map
+    return rule_map
 
 
-@to_rule_map.register
+@_recursive_to_rule_map.register
 def _(
         rule: Or,
-        _rule_map: nx.DiGraph = None,
-        _score=ShortCircuitScore(1, 1),
-        _combinable_rules: Dict[Hashable, Set[CombinableStardewRule]] = None
+        rule_map: nx.DiGraph,
+        score,
+        combinable_rules: Dict[Hashable, Set[CombinableStardewRule]],
+        root: bool = False
 ) -> nx.DiGraph:
-    root = False
-    if _rule_map is None:
-        _rule_map = nx.DiGraph()
-        _combinable_rules = {}
-        root = True
-
-    if rule in _rule_map.nodes:
-        _rule_map.nodes[rule]["score"] += _score
+    if rule in rule_map.nodes:
+        rule_map.nodes[rule]["score"] += score
     else:
-        _rule_map.add_node(rule, priority=0, score=_score)
+        rule_map.add_node(rule, priority=0, score=score, root=root)
 
     subrules = rule.original_rules
-    propagated_score = ShortCircuitScore(_score.true, _score.min / len(subrules))
+    propagated_score = ShortCircuitScore(score.true, score.min / len(subrules))
     for subrule in subrules:
-        to_rule_map(subrule, _rule_map, propagated_score, _combinable_rules)
-        _rule_map.add_edge(subrule, rule, propagation=ShortCircuitPropagation.POSITIVE)
+        _recursive_to_rule_map(subrule, rule_map, propagated_score, combinable_rules)
+        rule_map.add_edge(subrule, rule, propagation=ShortCircuitPropagation.POSITIVE)
 
-    if root:
-        _propagate_combinable_scores(_rule_map, _combinable_rules)
-
-    return _rule_map
+    return rule_map
 
 
-@to_rule_map.register
+@_recursive_to_rule_map.register
 def _(
         rule: Received,
-        _rule_map: nx.DiGraph = None,
-        _score=ShortCircuitScore(1, 1),
-        _combinable_rules: Dict[Hashable, Set[CombinableStardewRule]] = None
+        rule_map: nx.DiGraph,
+        score,
+        combinable_rules: Dict[Hashable, Set[CombinableStardewRule]],
+        *_
 ) -> nx.DiGraph:
-    if _rule_map is None:
-        _rule_map = nx.DiGraph()
-    elif rule in _rule_map.nodes:
-        _rule_map.nodes[rule]["score"] += _score
-        return _rule_map
+    if rule in rule_map.nodes:
+        rule_map.nodes[rule]["score"] += score
+        return rule_map
 
-    _rule_map.add_node(rule, priority=5, score=_score)
+    rule_map.add_node(rule, priority=5, score=score)
 
-    if _combinable_rules is not None:
-        _combinable_rules.setdefault(rule.combination_key, set()).add(rule)
+    if combinable_rules is not None:
+        combinable_rules.setdefault(rule.combination_key, set()).add(rule)
 
-    return _rule_map
+    return rule_map
 
 
-@to_rule_map.register
+@_recursive_to_rule_map.register
 def _(
         rule: HasProgressionPercent,
-        _rule_map: nx.DiGraph = None,
-        _score=ShortCircuitScore(1, 1),
-        _combinable_rules: Dict[Hashable, Set[CombinableStardewRule]] = None
+        rule_map: nx.DiGraph,
+        score,
+        combinable_rules: Dict[Hashable, Set[CombinableStardewRule]],
+        *_
 ) -> nx.DiGraph:
-    if _rule_map is None:
-        _rule_map = nx.DiGraph()
-    elif rule in _rule_map.nodes:
-        _rule_map.nodes[rule]["score"] += _score
-        return _rule_map
+    if rule in rule_map.nodes:
+        rule_map.nodes[rule]["score"] += score
+        return rule_map
 
-    _rule_map.add_node(rule, priority=4, score=_score)
+    rule_map.add_node(rule, priority=4, score=score)
 
-    if _combinable_rules is not None:
-        _combinable_rules.setdefault(rule.combination_key, set()).add(rule)
+    if combinable_rules is not None:
+        combinable_rules.setdefault(rule.combination_key, set()).add(rule)
 
-    return _rule_map
+    return rule_map
 
 
-@to_rule_map.register
+@_recursive_to_rule_map.register
 def _(
         rule: Reach,
-        _rule_map: nx.DiGraph = None,
-        _score=ShortCircuitScore(1, 1),
-        _combinable_rules: Dict[Hashable, Set[CombinableStardewRule]] = None
+        rule_map: nx.DiGraph,
+        score,
+        *_
 ) -> nx.DiGraph:
-    if _rule_map is None:
-        _rule_map = nx.DiGraph()
-    elif rule in _rule_map.nodes:
-        _rule_map.nodes[rule]["score"] += _score
-        return _rule_map
+    if rule_map is None:
+        rule_map = nx.DiGraph()
+    elif rule in rule_map.nodes:
+        rule_map.nodes[rule]["score"] += score
+        return rule_map
 
     # Reach can trigger cache refresh, which takes time... So, it's better to avoid it, hence the priority at 1.
-    _rule_map.add_node(rule, priority=1, score=_score)
+    rule_map.add_node(rule, priority=1, score=score)
 
-    return _rule_map
+    return rule_map
 
 
 def _propagate_combinable_scores(rule_map: nx.DiGraph, combinable_rules: Dict[Hashable, Set[CombinableStardewRule]]):
@@ -270,50 +275,39 @@ def _propagate_combinable_scores(rule_map: nx.DiGraph, combinable_rules: Dict[Ha
                     rule_map.nodes[other_rule]["score"] += ShortCircuitScore(score.true, 0)
 
 
-def to_evaluation_tree(rule_map: nx.DiGraph, root: BaseStardewRule) -> Node:
+def to_evaluation_tree(root: BaseStardewRule) -> Node:
     """ Precalculate the evaluation tree based on the possible results of each rule (recursively).
     Going from the root to one leaf should evaluate all the rules, as long as the leftovers are evaluated afterward.
-
-    TODO New algo to create evaluation tree
-        Find the node that can short-circuit the most rules. Sum true short-circuit rules and false short-circuit rules.
-        If decomposable (And and Or), add to killed nodes and
-
-    TODO handle weights ? -> Keep all nodes with equal links so center see the other rules. Add weight on equal link depending on point.
-        Add node matching exact rule to true_, so weight of that node can be considered.
-        Build graph by calling simplify_knowing() on each equal node.
-        Then we could evaluate one state subrule at the time. Prioritize received rule.
-
     :returns: The root node.
     """
 
-    ordered_nodes = sorted(rule_map.nodes.items(), key=lambda x: (x[1]["priority"], x[1]["score"].total))
-    rules = (node[0] for node in ordered_nodes)
-
-    return _recursive_create_evaluation_tree(deque(rules), root, AssumptionState())
+    return _recursive_create_evaluation_tree(root, AssumptionState())
 
 
-def _recursive_create_evaluation_tree(evaluation_queue: Deque[BaseStardewRule], root: BaseStardewRule, assumption_state: AssumptionState) -> Node:
-    if isinstance(root, LiteralStardewRule):
+def _recursive_create_evaluation_tree(root: BaseStardewRule, assumption_state: AssumptionState) -> Node:
+    root = root.deep_simplify_knowing(assumption_state)
+    rule_map = to_rule_map(root)
+    if rule_map.number_of_nodes() == 1:
         return Node.leaf(root)
 
-    evaluation_queue = evaluation_queue.copy()
-    center_rule = evaluation_queue.pop()
-    if not evaluation_queue:
-        return Node.leaf(center_rule)
+    most_significant_rule = max(rule_map.nodes.items(), key=lambda x: (x[1]["priority"], x[1]["score"].total))[0]
 
-    # FALSE branch
-    false_assumption_state = assumption_state.add_upper_bounds(center_rule)
-    false_simplified_root = root.deep_simplify_knowing(false_assumption_state)
-
-    false_evaluation_tree = _recursive_create_evaluation_tree(evaluation_queue, false_simplified_root, false_assumption_state)
+    false_assumption_state = assumption_state.add_upper_bounds(most_significant_rule)
+    false_evaluation_tree = _recursive_create_evaluation_tree(root, false_assumption_state)
     false_edge = Edge.simple_edge(false_evaluation_tree)
 
-    # TRUE branch
-
-    true_assumption_state = assumption_state.add_lower_bounds(center_rule)
-    true_simplified_root = root.deep_simplify_knowing(true_assumption_state)
-
-    true_evaluation_tree = _recursive_create_evaluation_tree(evaluation_queue, true_simplified_root, true_assumption_state)
+    true_assumption_state = assumption_state.add_lower_bounds(most_significant_rule)
+    true_evaluation_tree = _recursive_create_evaluation_tree(root, true_assumption_state)
     true_edge = Edge.simple_edge(true_evaluation_tree)
 
-    return Node(true_edge, false_edge, center_rule)
+    return Node(true_edge, false_edge, most_significant_rule)
+
+
+def to_optimized(rule: StardewRule) -> StardewRule:
+    # TODO allow Count, multiply score by weight of rule
+    if not isinstance(rule, (And, Or)):
+        return rule
+    rule = cast(BaseStardewRule, rule)
+
+    evaluation_tree = to_evaluation_tree(rule)
+    return OptimizedStardewRule(rule, evaluation_tree)
